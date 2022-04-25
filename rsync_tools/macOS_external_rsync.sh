9d73e32126afaeb035b2cd489d212db3eba6aa8e
petrellaperspective <103224625+petrellaperspective@users.noreply.github.com>
2022-04-25 13:26:37 -0500

    added rsyncs script and readme

---
+++ rsync_tools/macOS_external_rsync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Sudo /Volumes/Macintosh\ HD/usr/bin/rsync -avphtr --stats --progress --log-file=/Volumes/Macintosh\ HD/Users/localadmin/Desktop/rsync_log.txt 
---
+++ rsync_tools/readme.md
@@ -0,0 +1,1 @@
+This script runs the built-in rsync tool to lay the migrate files from the external drive to the user's directory on the current machine
